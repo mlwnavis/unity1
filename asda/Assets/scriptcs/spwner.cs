2830da85a37e58781d8ac42a7b4104c8004465a4
Szkolenia <szkolenia@studzpsb.local>
2017-04-13 18:47:27 +0200

    de

---
+++ asda/Assets/scriptcs/spwner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spwner : MonoBehaviour {
+
+	public GameObject Box;
+	[SerializeField]
+	private GameObject _altbox;
+
+    void awake()
+    {
+
+    }
+	// Use this for initialization
+	void Start () {
+		GameObject _altbox = Resources.Load("Box") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Space")) {
+			Instantiate (_altbox, this.transform.position, Quaternion.identity);	
+		}
+	}
+}
